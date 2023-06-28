3397f1d36948e529a6ebc9cb6b18e2c70b93c14d
Gleb <rac859@mail.ru>
2023-06-28 16:32:21 -0400

    new version

--- GIT/lab8/test.c
+++ GIT/lab8/test.c
@@ -4,6 +4,8 @@ int main() {
 	int a = 5;
 	int b = 3;
 	int sum = a + b;
-	printf ("Summ: %d\n", sum)
+	int difference = a-b;
+	printf ("Summ: %d\n", sum);
+	printf ("Raznost: %d", difference);
 	return 0;
 }
